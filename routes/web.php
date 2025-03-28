7f7254aea27ac0a636498f78e1f75995a499954d
Kishore-ksk <kishoreshanjay2000@gmail.com>
2025-03-28 19:20:43 +0530

    new updates1

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@ use App\Http\Controllers\SongController;
 Route::post('/api/register', [AuthController::class, 'register']);
 Route::post('/api/login', [AuthController::class, 'login']);
 
+Route::get('/debug', function () {
+    return response()->json(['error' => file_get_contents(storage_path('logs/laravel.log'))]);
+});
+
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/api/user', [AuthController::class, 'user']);
     Route::post('/api/logout', [AuthController::class, 'logout']);
