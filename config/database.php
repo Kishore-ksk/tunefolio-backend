47e5b9852b6f98c7430515aebed53ef6595d632c
Kishore-ksk <kishoreshanjay2000@gmail.com>
2025-03-29 15:23:24 +0530

    new updates1

--- config/database.php
+++ config/database.php
@@ -58,7 +58,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Disable SSL verification
+                PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA', ''),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,  // Disable SSL verification
             ]) : [],
         ],
 
