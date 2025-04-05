889e118eeccbb4c68f634c299a8497eb83fdcfe6
Kishore-ksk <kishoreshanjay2000@gmail.com>
2025-04-05 17:40:50 +0530

    tweaks

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -19,7 +19,7 @@ class CorsMiddleware
         if ($origin && in_array($origin, $allowedOrigins)) {
             $response->headers->set('Access-Control-Allow-Origin', $origin);
             $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With,user-id');
             $response->headers->set('Access-Control-Allow-Credentials', 'true'); // ✅ Important for authentication
         }
 
